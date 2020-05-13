87514cd65a3cbd69541924f597018d815f5be447
NomenBirch <wrhcodingbootcamp@gmail.com>
2020-05-13 15:56:56 -0500

    added work

---
+++ index.js
@@ -0,0 +1,27 @@
+object.onclick = function makePassword() {
+	let length = "";
+
+	let special = "!@#$%^&*()/?<>,.|-_=+";
+	let upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
+	let lower = "qwertyuiopasdfghjklzxcvbnm";
+	let digits = "0123456789";
+
+	let password = "";
+
+	/*for (var i = 0; i < vegetables.length; i++) {
+    console.log("I love " + vegetables[i]);
+  }*/
+
+	let str =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789@#$";
+};
+var length = prompt(
+	"Please enter password length",
+	"8-128 characters ONLY INTEGERS"
+);
+
+if (length >= 8 && length <= 128 && Number.isInteger(length)) {
+	alert(length);
+} else {
+	alert("Please pick a number between 8-128 that is an integer");
+}
