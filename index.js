6a1cb604e3847f46080e9d4c6bfd9765171fbac4
NomenBirch <wrhcodingbootcamp@gmail.com>
2020-05-25 09:59:38 -0500

    added updates, the variable isnt logging

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var theButton = document.getElementById("passBtn");
-
+var passLength = "";
 function passAskLength() {
-	prompt("Hey this is a test.");
+	passLength = prompt("Enter your desired password length between 8-128");
+	console.log(passLength);
+}
+// now we have to make pass length into an int so it can be validated this fixes why it didnt work last time
+
+var passLengthInt = parseInt(passLength);
+
+if (
+	passLength !== "" &&
+	passLengthInt >= 8 &&
+	passLengthInt <= 128 &&
+	Number.isInteger(passLengthInt === true)
+) {
+	alert("your pass is" + passLengthInt + "characters long");
 }
